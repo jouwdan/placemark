d179b03f371e72953ebebd47edc50bb0a109bbde
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-02-15 22:19:47 +0000

    initial commit

---
+++ src/controllers/dashboard-controller.js
@@ -0,0 +1,7 @@
+export const dashboardController = {
+    index: {
+      handler: async function (request, h) {
+        return h.view("main");
+      },
+    },
+  };
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "jit",
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  },
+}
