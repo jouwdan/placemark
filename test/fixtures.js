817b1a1c7549756bd1b545894145d27147bd1752
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-02-26 13:36:32 +0000

    added tests

---
+++ test/fixtures.js
@@ -0,0 +1,27 @@
+export const maggie = {
+    firstName: "Maggie",
+    lastName: "Simpson",
+    email: "maggie@simpson.com",
+    password: "secret"
+  };
+  
+  export const testUsers = [
+    {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret"
+    },
+    {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret"
+    },
+    {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret"
+    }
+  ];
