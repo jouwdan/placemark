39660232a90f86c45b57686e207d6309de97aded
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-03-15 22:10:56 +0000

    add user tests

---
+++ test/fixtures.js
@@ -0,0 +1,42 @@
+export const serviceUrl = "http://localhost:3000";
+
+export const maggie = {
+  firstName: "Maggie",
+  lastName: "Simpson",
+  email: "maggie@simpson.com",
+  password: "secret",
+  role: "User"
+};
+
+export const testUsers = [
+  {
+    firstName: "Homer",
+    lastName: "Simpson",
+    email: "homer@simpson.com",
+    password: "secret",
+    role: "Administrator"
+  },
+  {
+    firstName: "Marge",
+    lastName: "Simpson",
+    email: "marge@simpson.com",
+    password: "secret",
+    role: "User"
+  },
+  {
+    firstName: "Bart",
+    lastName: "Simpson",
+    email: "bart@simpson.com",
+    password: "secret",
+    role: "User"
+  }
+];
+
+export const lana = {
+    name: "Lana Portlaoise",
+    description: "LANA is a fusion of authentic Asian recipes and modern flavours.",
+    category: "Casual Dining",
+    cuisine: "Thai",
+    longitude: 53.03452056851195,
+    latitude: -7.299739603701025
+};
