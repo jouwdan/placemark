4f29f7ee8aa14aa350cb3ca03956987ddb3f11d7
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2022-03-22 21:56:42 +0000

    enable heroku deployment

--- src/server.js
+++ src/server.js
@@ -31,8 +31,7 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
   await server.register([
     Vision,
